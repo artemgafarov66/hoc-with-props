ea761f90be164361e38e4560e6254cdd63ecb3cc
artemgafarov <artem.r.gafarov@megafon.ru>
2020-10-14 10:26:00 +0300

    fix v.1.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,34 @@
-import {
-  createElement,
-  forwardRef,
-  ComponentClass,
-  FunctionComponent
-} from "react";
+import { createElement, forwardRef, ElementType } from "react";
 
 declare type Fn = (props: object) => object;
-declare type Component = string | ComponentClass | FunctionComponent;
 
-const getDisplayName = (Component: Component) => {
-  if (typeof Component === "string") {
-    return Component;
-  }
+const getDisplayName = (Component: ElementType) =>
+  typeof Component === "string"
+    ? Component
+    : !Component
+    ? undefined
+    : Component.displayName || Component.name || "Component";
 
-  if (!Component) {
-    return undefined;
-  }
-
-  return Component.displayName || Component.name || "Component";
-};
-
-const mapProps = (propsMapper: Fn) => (BaseComponent: Component) =>
-  forwardRef((props: object, ref) => {
-    return createElement(BaseComponent, propsMapper({ ...props, ref }));
-  });
+const mapProps = (propsMapper: Fn) => (BaseComponent: ElementType) =>
+  forwardRef((props: object, ref) =>
+    createElement(BaseComponent, propsMapper({ ...props, ref }))
+  );
 
 const withProps = (input: Fn | object) => {
-  const hoc = mapProps(props => ({
-    ...(typeof input === "function" ? input(props) : input),
-    ...props
-  }));
+  const result = mapProps(props =>
+    typeof input === "function"
+      ? { ...props, ...input(props) }
+      : {
+          ...input,
+          ...props
+        }
+  );
 
-  return (BaseComponent: Component) => {
-    BaseComponent["displayName"] = `withProps(${getDisplayName(
-      BaseComponent
-    )})`;
+  return (BaseComponent: ElementType) => {
+    const hoc = result(BaseComponent);
+    hoc.displayName = `withProps(${getDisplayName(BaseComponent)})`;
 
-    return hoc(BaseComponent);
+    return hoc;
   };
 };
 
